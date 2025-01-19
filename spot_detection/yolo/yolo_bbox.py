5dd7f1cd632b523863d812ee2d322a7db64be9b9
friedawik <friedawik@gmail.com>
2025-01-19 21:07:43 +0100

    add spot detection codes

---
+++ spot_detection/yolo/yolo_bbox.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+from IPython import embed
+
+# Load a pretrained model
+model = YOLO("yolo11n.pt")
+
+# Train the model
+results = model.train(data="../../data/patch_256_bbox/dataset.yaml", 
+                        batch = 4, 
+                        epochs=5, 
+                        lr0=0.05, 
+                        imgsz=256,
+                        mask_ratio=1,
+                        cls=0.1
+                        )
+
+metrics = model.val()
+
+# Print performance metrics
+print(metrics.results_dict)
