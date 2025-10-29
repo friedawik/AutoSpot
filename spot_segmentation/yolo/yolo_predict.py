0e19dbf88765e6b8f0ffe2b0abb0ba6cfd6a4650
Frieda Wik <frieda@g001.cm.cluster>
2025-10-29 21:51:16 +0100

    update sam

--- spot_segmentation/yolo/yolo_predict.py
+++ spot_segmentation/yolo/yolo_predict.py
@@ -2,20 +2,20 @@ from ultralytics import YOLO
 from ultralytics.utils.benchmarks import benchmark
 from IPython import embed
 
-# Load a pretrained YOLO11n model
-# model = YOLO("yolo11n-seg.pt")
+"""
+Predict with finetuned YOLO11 segmentation model.
+"""
+# Load finetuned model
 model = YOLO("runs/segment/train3/weights/best.pt")
-# img = '../../data/patch_256/test/images/MF_MaxIP_3ch_2_000_230623_544_84_F_XY4_x0_y1.tif'
-# img = '../../data/patch_256_8bit/test/images/MF_MaxIP_3ch_2_000_230623_544_84_F_XY4_x0_y1.png'
-# Run inference on 'bus.jpg' with arguments
-# results = model.predict(img, save=True, imgsz=256, conf=0.3)
-# metrics = model.val(data="../../data/patch_256_8bit/dataset.yaml", save_json=True)
-# embed()
-metrics = model.val(data="../../data/patch_256_8bit/dataset.yaml",split="test", plots=True)
-embed()
 
+# Get image
+img = '../../data/patch_256_8bit/test/images/MF_MaxIP_3ch_2_000_230623_544_84_F_XY4_x0_y1.png'
+
+# Run inference
+results = model.predict(img, save=True, imgsz=256, conf=0.3)
 
 # Get segmentation performance
+metrics = model.val(data="../../data/patch_256_8bit/dataset.yaml",split="test", plots=True)
 print(metrics.results_dict)
 
 
